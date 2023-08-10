d5b65dc4534771cfb2245b051513db838ceb6efd
DG-W <141761198+DG-W@users.noreply.github.com>
2023-08-10 14:07:11 -0500

    Update rmgpuburn.sh

--- rmgpuburn.sh
+++ rmgpuburn.sh
@@ -1,10 +1,8 @@
-sudo -i
-docker rmi $(docker images -a -q)
-apt remove docker.io
-apt remove nvidia-container-toolkit
-apt-get purge docker docker.io
-apt-get purge nvidia-container-toolkit-base
+sudo docker rmi $(docker images -a -q)
+sudo apt remove docker.io
+sudo apt remove nvidia-container-toolkit
+sudo apt-get purge docker docker.io
+sudo apt-get purge nvidia-container-toolkit-base
 
-logout
 cd
 rm -rf gpu-burn
