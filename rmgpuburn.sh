beb276211f6b15d5523e4461a6a2e4601462e737
DG-W <141761198+DG-W@users.noreply.github.com>
2026-08-05 08:52:10 -0500

    Update rmgpuburn.sh
    Added sudo apt clean and cleaning up Nvidia Toolkit repository .list and .gpg

--- rmgpuburn.sh
+++ rmgpuburn.sh
@@ -4,6 +4,10 @@ sudo apt remove -y nvidia-container-toolkit
 sudo apt-get purge -y docker docker.io
 sudo apt-get purge -y nvidia-container-toolkit-base
 sudo apt autoremove -y
+sudo apt clean
+
+sudo rm -f /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo rm -f /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 
 cd
 rm -rf gpu-burn
